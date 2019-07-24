df9f40a3657a64a60f3b09c976560cb2dd0dec52
simonhughcom <simonhughdev@gmail.com>
2019-07-24 14:45:12 +0100

    config

--- config.h
+++ config.h
@@ -18,4 +18,4 @@ static const char * message = "Suckless: Software that sucks less.";
 static const char * text_color = "#ffffff";
 
 /* text size (must be a valid size) */
-static const char * text_size = "6x10";
+static const char * text_size = "60x100";
